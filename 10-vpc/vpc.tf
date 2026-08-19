474188b4641d1ee259eeb02d940fa198e5ec823d
sairamkspj <sairamkspj@gmail.com>
2026-08-19 12:01:18 +0530

    Update vpc.tf

--- 10-vpc/vpc.tf
+++ 10-vpc/vpc.tf
@@ -1,6 +1,6 @@
 module "vpc" {
     #source = "../terraform-aws-vpc"
-    source = "git::https://github.com/daws-81s/terraform-aws-vpc.git?ref=main"
+    source = "git::https://github.com/sairamkspj/terraform-aws-vpc.git?ref=main"
     vpc_cidr = var.vpc_cidr
     project_name = var.project_name
     environment = var.environment
